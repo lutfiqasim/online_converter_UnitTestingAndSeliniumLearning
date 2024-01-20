6c6c9c82688f30d23f1c076ded00ea2aa3f32090
lutfiqasim <lutfiqasim2002@gmail.com>
2024-01-20 12:24:21 +0200

    done some refactoring and added a few more tests

--- src/test/java/com/softwareTesting/online_converter/TestFile.java
+++ src/test/java/com/softwareTesting/online_converter/TestFile.java
@@ -1,25 +1,31 @@
 package com.softwareTesting.online_converter;
 
-
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Optional;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class TestFile {
 
+    @Mock
+    CurrencyRepository currencyRepository;
+    @InjectMocks
+    CurrencyController currencyController;
 
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/rate.csv")
@@ -27,24 +33,31 @@ public class TestFile {
     @DisplayName("Test reading data from CSV file")
     void testReadDataFromCSV(String fromCurrency, String toCurrency, double expectedExchangeRate) {
         // Given
+        Currency cur1 = new Currency(fromCurrency, "cur1");
+        Currency cur2 = new Currency(toCurrency, "cur2");
+
+        // Mock the repository to return instances of Currency
+        when(currencyRepository.findById(fromCurrency)).thenReturn(Optional.of(cur1));
+        when(currencyRepository.findById(toCurrency)).thenReturn(Optional.of(cur2));
+        when(currencyRepository.findAll()).thenReturn(List.of(cur1, cur2));
+        currencyController = new CurrencyController(currencyRepository);
+
         ExchangeRate exchangeRate = new ExchangeRate(fromCurrency, toCurrency, expectedExchangeRate);
-
-
-        exchangeRate.setExchangeRate(0.87);
 
         // When
         String actualFromCurrency = exchangeRate.getFromCurrencyName();
         String actualToCurrency = exchangeRate.getToCurrencyName();
         double actualExchangeRate = exchangeRate.getExchangeRate();
 
-
-
-
-
-//         Then
+        FromToResponse response = currencyController.successResponse(toCurrency, expectedExchangeRate, 100);
+        //Then
         assertEquals(fromCurrency, actualFromCurrency);
         assertEquals(toCurrency, actualToCurrency);
-        assertEquals(expectedExchangeRate, actualExchangeRate, 0.0001); // Adjust delta as needed
+        assertEquals(expectedExchangeRate, actualExchangeRate, 0.0001);
+        assertEquals(expectedExchangeRate, response.getExchangeRate());
+        if (response.getResult().contains(","))
+            response.setResult(response.getResult().replace(',', '.'));
+        assertEquals(100 * expectedExchangeRate, Double.parseDouble(response.getResult()), 0.5);
     }
 
 }
